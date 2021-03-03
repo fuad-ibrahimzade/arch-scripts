67941407b83025ff3e4c26d8a9d74737e1994568
fuad.i <fuad.i@agilesolutions.az>
2021-03-03 13:57:15 +0400

    fish fisher tide configs

---
+++ i3-seperate-install-config/fish/config.fish
@@ -0,0 +1,7 @@
+set -g -x fish_greeting ''
+### "vim" as manpager
+set -x MANPAGER '/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
+
+### "nvim" as manpager
+# set -x MANPAGER "nvim -c 'set ft=man' -"
+#xcompmgr -c -f -n
