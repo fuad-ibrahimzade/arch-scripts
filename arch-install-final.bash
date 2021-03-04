dca11dc8eb23c33c5596f857569600d01e8e0817
fuad.i <fuad.i@agilesolutions.az>
2021-03-04 17:45:57 +0400

    battery tint2

--- arch-install-final.bash
+++ arch-install-final.bash
@@ -324,7 +324,7 @@ EOF
 	cat temp >> "/home/$user_name/.vimrc"
 	rm temp
 
-	pacman -S --noconfirm abiword zathura zathura-pdf-mupdf zathura-djvu pulseaudio pavucontrol vlc xorg-xbacklight
+	pacman -S --noconfirm abiword zathura zathura-pdf-mupdf zathura-djvu pulseaudio pavucontrol vlc xorg-xbacklight acpi
 	# TODO remove
 	# git clone https://github.com/acaloiaro/di-tui
 	# cp di-tui/di-tui /usr/bin/di-tui 
