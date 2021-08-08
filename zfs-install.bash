3950bd681e805cab05bc91b42926935a7606a208
fuad-ibrahimzade <i.fuad.tm@gmail.com>
2021-08-08 12:48:27 +0400

    refactor

--- zfs-install.bash
+++ zfs-install.bash
@@ -355,7 +355,7 @@ initDefaultOptions() {
 
 	# echo "$Output_Device:$root_password:$user_name:$user_password:$filesystem:$offlineInstallUnsquashfs:$install_tools"
 	# echo "Output_Device=\"$Output_Device\"; root_password=\"$root_password\"; user_name=\"$user_name\"; user_password=\"$user_password\"; fil'esystem=\"$filesystem\"; offlineInstallUnsquashfs=\"$offlineInstallUnsquashfs\"; bootsystem=\"$bootsystem\"; install_tools=\"$install_tools\""
-	echo 'Output_Device='$Output_Device'; root_password='$root_password'; user_name='$user_name'; user_password='$user_password'; filesystem='$filesystem'; offlineInstallUnsquashfs='$offlineInstallUnsquashfs'; bootsystem='$bootsystem'; install_tools='$install_tools'
+	echo 'Output_Device='"$Output_Device"'; root_password='"$root_password"'; user_name='"$user_name"'; user_password='"$user_password"'; filesystem='"$filesystem"'; offlineInstallUnsquashfs='"$offlineInstallUnsquashfs"'; bootsystem='"$bootsystem"'; install_tools='"$install_tools"
 
 }
 
