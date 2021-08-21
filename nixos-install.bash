6c21ff0f51b36db56771bc8c0c19f0e87e97876c
fuad.i <fuad.i@agilesolutions.az>
2021-08-21 21:44:29 +0400

    refactor math

--- nixos-install.bash
+++ nixos-install.bash
@@ -187,7 +187,7 @@ initAndMountPartitions() {
 	sudo partprobe;
 
 	disk_size=$(sudo fdisk -l | grep Disk | grep Output_Device | awk -F"GiB" '{print $1}' | awk -F: '{print $2}')
-	if ((disk_size >16));then
+	if [[ "$disk_size" -gt 16 ]];then
 		sudo sgdisk -n 0:0:+4GiB -t 0:8200 -c 0:swap "$DISK"
 	else
 		sudo sgdisk -n 0:0:+1GiB -t 0:8200 -c 0:swap "$DISK"
@@ -196,7 +196,7 @@ initAndMountPartitions() {
 	sudo partprobe;
 
 	remaining_free_disk_size=$(sudo parted /dev/sda unit GiB print free | grep "Free Space" | awk -F"Free Space" '{print $(NF-1)}' | awk '{print $NF}' | tail -n -1 | awk -F"GiB" '{print $1}')
-	if ((remaining_free_disk_size > root_partitionsize));then
+	if [[ "$remaining_free_disk_size" -gt "$root_partitionsize" ]]; then
 		sudo sgdisk -n 0:0:+"$root_partitionsize"GiB -t 0:BF01 -c 0:ZFS "$DISK"
 	else
 		sgdisk -n 0:0:0 -t 0:BF01 -c 0:ZFS "$DISK"
